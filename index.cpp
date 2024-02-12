e16e90214f0733cab889e1817b4a9241cde61734
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2024-02-12 17:57:27 +0530

    dsa first

---
+++ index.cpp
@@ -0,0 +1,58 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int a; //declaration
+//     a=5; //initialization
+//     float b=5.9;
+//     bool c=true;
+//     cout << "value of a is " << a <<endl; 
+//     cout << "value of b is " << b <<endl;
+//     cout << "value of c is " << c;
+
+//     return 0;
+// }
+
+// #include <iostream>
+// #define MAX 10;
+// using namespace std;
+
+// int main(){
+
+//     cout << MAX;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+    
+// int a=2;
+// int b=8;
+
+
+// cout << "sum is " <<a+b;
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+    
+// int a;
+// int b;
+
+// cout << "value of a ";
+// cin >> a;
+// cout << "value of b ";
+// cin >> b;
+
+// cout << "sum is " <<a+b << endl;
+// cout <<"god bye";
+//     return 0;
+// }
